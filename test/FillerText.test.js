1a233d45a8f725f7b037ce10c60775f8a0a7370a
Luke Ghenco <lukeghenco@gmail.com>
2017-06-02 09:43:55 -0400

    Update tests to use chai

--- test/FillerText.test.js
+++ test/FillerText.test.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
-import FillerText from '../components/FillerText';
+import FillerText from '../src/components/FillerText';
 
 describe('<FillerText />', function () {
   const wrapper = shallow(<FillerText />);
 
   it('should have the right tag', function () {
-    expect(wrapper.type()).toEqual('p', (res) => `\`${res[1]}\` is not the right element tag.`);
+    expect(wrapper.type()).to.equal('p', (res) => `\`${res[1]}\` is not the right element tag.`);
   });
 
   it('should have the right content', function () {
-    expect(wrapper.text()).toEqual('I am a filler text. I can be used to fill your screen. Amazing!');
+    expect(wrapper.text()).to.equal('I am a filler text. I can be used to fill your screen. Amazing!');
   });
 });
--- test/RegistrationForm.test.js
+++ test/RegistrationForm.test.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
-import RegistrationForm from '../components/RegistrationForm';
+import RegistrationForm from '../src/components/RegistrationForm';
 
 describe('<RegistrationForm />', function () {
   const wrapper = shallow(<RegistrationForm />);
 
   it('should have the right tag', function () {
-    expect(wrapper.type()).toEqual('form', (res) => `\`${res[1]}\` is not the right element tag.`);
+    expect(wrapper.type()).to.equal('form', (res) => `\`${res[1]}\` is not the right element tag.`);
   });
 
   it('should contain one text input', function () {
-    expect(wrapper.find('input[type="text"]').length).toEqual(1);
+    expect(wrapper.find('input[type="text"]').length).to.equal(1);
   });
 
   it('should contain one password input', function () {
-    expect(wrapper.find('input[type="password"]').length).toEqual(1);
+    expect(wrapper.find('input[type="password"]').length).to.equal(1);
   });
 
   it('should contain one submit button', function () {
-    expect(wrapper.find('button[type="submit"]').length).toEqual(1);
+    expect(wrapper.find('button[type="submit"]').length).to.equal(1);
   });
 });
